1175f6d7481e16ef5b78aedeb58d5ece31d09248
sujay106526 <sujay.1si21cs106@gmail.com>
2025-05-18 14:50:29 +0530

    feat: add pagination utility and integrate with routes
    - Implemented `getPagination` function for handling pagination parameters.
    - Created `prismaClient` for database interactions.
    - Refactored authentication routes to use new error handling and added additional fields for sign-up.
    - Updated comments routes to include pagination and improved error handling.
    - Enhanced likes routes with session middleware and improved error responses.
    - Introduced session middleware for user authentication and session management.
    - Removed token middleware and replaced it with session middleware across routes.
    - Refactored posts routes to utilize pagination and improved error handling.
    - Added Swagger documentation routes for API documentation.
    - Updated user routes to support fetching and updating user information with pagination.

--- src/routes/middlewares/token-middleware.ts
+++ src/routes/middlewares/token-middleware.ts
@@ -1,33 +1,33 @@
-import { createMiddleware } from "hono/factory";
-import * as jwt from "jsonwebtoken";
-import { jwtsecretKey } from "../../../environment";
+// import { createMiddleware } from "hono/factory";
+// import jwt from "jsonwebtoken";
+// import { jwtSecretKey } from "../../environment.js";
 
-export const tokenMiddleware = createMiddleware<{
-  Variables: {
-    userId: string;
-  };
-}>(async (context, next) => {
-  const token = context.req.header("token");
-  if (!token) {
-    return context.json(
-      {
-        message: "missing Token",
-      },
-      401
-    );
-  }
+// export const tokenMiddleware = createMiddleware<{
+//   Variables: {
+//     userId: string;
+//   };
+// }>(async (context, next) => {
+//   const token = context.req.header("token");
 
-  try {
-    const payload = jwt.verify(token, jwtsecretKey) as jwt.JwtPayload;
+//   if (!token) {
+//     return context.json(
+//       {
+//         message: "missing Token",
+//       },
+//       401
+//     );
+//   }
 
-    const userId = payload.sub;
+//   try {
+//     const payload = jwt.verify(token, jwtSecretKey) as jwt.JwtPayload;
+//     const userId = payload.sub;
 
-    if (userId) {
-      context.set("userId", userId);
-    }
+//     if (userId) {
+//       context.set("userId", userId);
+//     }
 
-    await next();
-  } catch (e) {
-    return context.json({ message: "Unauthorized token" }, 401);
-  }
-});
+//     await next();
+//   } catch (e) {
+//     return context.json({ message: "Unauthorized token" }, 401);
+//   }
+// });
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,13 +1,33 @@
 import { Hono } from "hono";
-import { authenticationRoutes } from "./authentication-routes";
-import { usersRoutes } from "./users-routes";
-import { postsRoutes } from "./posts-routes";
-import { likesRoutes } from "./likes-routes";
-import { commentsRoutes } from "./comments-routes";
+import { swaggerUI } from "@hono/swagger-ui";
+
+import { authenticationRoutes } from "./authentication-routes.js";
+import { usersRoutes } from "./users-routes.js";
+import { postsRoutes } from "./posts-routes.js";
+import { likesRoutes } from "./likes-routes.js";
+import { commentsRoutes } from "./comments-routes.js";
+
+import { cors } from "hono/cors";
+import { authRoute } from "./middlewares/session-middleware.js";
+import { webClientUrl } from "../environment.js";
 
 export const allRoutes = new Hono();
 
-allRoutes.route("/auth", authenticationRoutes);
+allRoutes.use(
+  cors({
+    origin: [webClientUrl],
+    allowMethods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization", "token"],
+    exposeHeaders: ["Content-Length"],
+    credentials: true,
+    maxAge: 600,
+  })
+);
+
+allRoutes.get("/ui", swaggerUI({ url: "/docs" }));
+
+allRoutes.route("/api/auth", authRoute);
+allRoutes.route("/authen", authenticationRoutes);
 allRoutes.route("/users", usersRoutes);
 allRoutes.route("/posts", postsRoutes);
 allRoutes.route("/likes", likesRoutes);
